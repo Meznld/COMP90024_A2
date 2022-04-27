47b757488293112b9384f8b4436eb308e4ae7430
Tony <66058518+tonywongthw@users.noreply.github.com>
2022-04-27 16:46:19 +1000

    updated rouse of virtual environment

--- simpe Map/venv/Scripts/activate.bat
+++ simpe Map/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tony1\Dropbox\Git\Cluster And Cloud\Flask\simpe Map\venv
+set VIRTUAL_ENV=C:\Users\tony1\Documents\GitHub\COMP90024_A2\simpe Map\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
