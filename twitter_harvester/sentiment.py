0aae990d897fb981ae0f0442de9a7235a1291ea7
belkok <belkok@hotmail.sg>
2022-04-28 11:25:14 +1000

    sentiment

--- twitter_harvester/sentiment.py
+++ twitter_harvester/sentiment.py
@@ -3,48 +3,46 @@ import string
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-nltk.download('stopwords')
+
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from emot.emo_unicode import UNICODE_EMO, EMOTICONS
+from emot.emo_unicode import UNICODE_EMOJI
 
 
-from sklearn.feature_extraction.text import CountVectorizer
 
 stop_words=stopwords.words('english')
 
 def convert_emojis(text):
-    for emot in UNICODE_EMO:
-        text = text.replace(emot, "_".join(UNICODE_EMO[emot].replace(",","").replace(":","").split()))
+    for emot in UNICODE_EMOJI:
+        text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",","").replace(":","").split()))
+    text = re.sub("_"," ",text)
     return text
 
-def convert_emoticons(text):
-    for emot in EMOTICONS:
-        text = re.sub(u'('+emot+')', "_".join(EMOTICONS[emot].replace(",","").split()), text)
-    return text
 
-def preprosess_tweet(tweet):
-    # remove urls
-    tweet = re.sub(r"http\S+|www\S+|https\S+", '', tweet, flags=re.MULTILINE)
+def preprocess_tweet(tweet):
+    # remove urls and links
+    tweet = re.sub(r"http\S+|www\S+|https\S+|bit.ly/\S+'", '', tweet, flags=re.MULTILINE)
+    tweet = tweet.strip('[link]')
+    tweet = re.sub(r'pic.twitter\S+','', tweet)
+
+    # remove retweet, hashtags and @user
+    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet) 
+    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
+    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
 
     # convert emojis and emoticon to words
     try:
         tweet = convert_emojis(tweet)
     except:
         tweet = tweet
-    try: 
-        tweet = convert_emoticons(tweet)
-    except:
-        tweet = tweet
 
-    # remove hashtags and mentions
-    tweet = re.sub(r'\@\w+|\#','', tweet)
     # remove multiple spaces
     tweet = re.sub("\s+"," ",tweet)
+   
     # remove punctuations
     tweet = tweet.translate(str.maketrans('', '', string.punctuation))
     # convert to lower case
-    tweet.lower()
+    
 
     tweet_tokens = word_tokenize(tweet)
     result = [w for w in tweet_tokens if not w in stop_words]
@@ -52,5 +50,6 @@ def preprosess_tweet(tweet):
     lemmawords = [lemmatizer.lemmatize(w) for w in result]
     return " ".join(lemmawords)
 
-sentence = """At eight o'clock on Thursday morning
-... Arthur didn't feel very good."""
+sentence = """At eight o'clock on Thursday morning \n
+... Arthur didn't feel very good. 😂 www.help.com"""
+print(preprocess_tweet(sentence))
