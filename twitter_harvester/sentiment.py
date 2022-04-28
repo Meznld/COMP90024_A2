8ea799a3c27ebec3d27d9f4ba3f70be5a9eed5eb
belkok <belkok@hotmail.sg>
2022-04-28 14:14:06 +1000

    Update sentiment.py

--- twitter_harvester/sentiment.py
+++ twitter_harvester/sentiment.py
@@ -3,53 +3,61 @@ import string
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from emot.emo_unicode import UNICODE_EMOJI
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import couchdb
 
 
+couch = couchdb.Server('http://admin:XlkLSNezrwOlQ0fIx5C6@172.26.128.201:30396/')
+db = couch['augment']
+test = []
+for idx,val in enumerate(db):
+    if idx < 20:
+
+        test.append(db[val]['text'])
 
 stop_words=stopwords.words('english')
+sid = SentimentIntensityAnalyzer()
 
-def convert_emojis(text):
-    for emot in UNICODE_EMOJI:
-        text = text.replace(emot, "_".join(UNICODE_EMOJI[emot].replace(",","").replace(":","").split()))
-    text = re.sub("_"," ",text)
-    return text
+def polarity_score(compound):
+   
+    if compound == 0.0:
+        return "neutral"
+    elif compound < 0:
+        return "negative"
+    elif compound > 0:
+        return "positive"
 
-
-def preprocess_tweet(tweet):
+def preprocess_tweet(raw):
     # remove urls and links
-    tweet = re.sub(r"http\S+|www\S+|https\S+|bit.ly/\S+'", '', tweet, flags=re.MULTILINE)
+    tweet = re.sub(r"http\S+|www\S+|https\S+|bit.ly/\S+'", '', raw, flags=re.MULTILINE)
     tweet = tweet.strip('[link]')
     tweet = re.sub(r'pic.twitter\S+','', tweet)
 
     # remove retweet, hashtags and @user
-    tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet) 
-    tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
-    tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', tweet)
-
-    # convert emojis and emoticon to words
-    try:
-        tweet = convert_emojis(tweet)
-    except:
-        tweet = tweet
+    tweet = re.sub(r"RT @[\w]*:", ' ', tweet, flags=re.MULTILINE)
+    tweet = re.sub(r"#(\w+)", ' ', tweet, flags=re.MULTILINE)
+    tweet = re.sub(r"@(\w+)", ' ', tweet, flags=re.MULTILINE)
 
     # remove multiple spaces
     tweet = re.sub("\s+"," ",tweet)
-   
-    # remove punctuations
-    tweet = tweet.translate(str.maketrans('', '', string.punctuation))
-    # convert to lower case
-    
 
-    tweet_tokens = word_tokenize(tweet)
-    result = [w for w in tweet_tokens if not w in stop_words]
-    lemmatizer = WordNetLemmatizer()
-    lemmawords = [lemmatizer.lemmatize(w) for w in result]
-    return " ".join(lemmawords)
+    scores = sid.polarity_scores(tweet)
+    classify = polarity_score(scores['compound'])
+    return print(tweet + "\n", scores, classify)
 
-sentence = """At eight o'clock on Thursday morning \n
-... Arthur didn't feel very good. 😂 www.help.com"""
-print(preprocess_tweet(sentence))
+
+
+
+# sentence = """ 😩 At eight o'clock on Thursday morning \n
+# ... Arthur didn't feel this very good.  www.help.com RT @aodsufh"""
+# # print(preprocess_tweet(sentence))
+# emoti = "i hate this"
+# print(preprocess_tweet(sentence))
+# # print(sentiment(preprocess_tweet(sentence)))
+
+for i in test:
+
+    preprocess_tweet(i)
+
