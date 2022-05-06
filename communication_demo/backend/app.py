09e429a68fea6419e9075f27b1d6fd599d9b8533
Tony <66058518+tonywongthw@users.noreply.github.com>
2022-05-06 16:04:28 +1000

    updated backend API endpoints

--- communication_demo/backend/app.py
+++ communication_demo/backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import requests
 import json
 from flask_cors import CORS
 
@@ -20,6 +21,50 @@ def get_filedata():
 def hello_world():
     return ("Hello!")
 
+@app.route("/testGet")
+def testGet():
+    uri = "http://172.26.132.238:5984/"
+    try:
+        uResponse = requests.get(uri)
+        Jresponse = uResponse.text
+        data = json.loads(Jresponse)
+        output = data
+    except requests.ConnectionError:
+       return "Connection Error"
+
+    return output
+
+@app.route("/testGetSpecific/<str>")
+def testGetSpecific(str):
+    uri = "http://172.26.128.201:30396/"
+    try:
+        uResponse = requests.get(uri)
+        Jresponse = uResponse.text
+        data = json.loads(Jresponse)
+        output = data[str]
+    except requests.ConnectionError:
+       return "Connection Error"
+
+    return output
+
+@app.route("/testGet2")
+def testGet2():
+    uri = "http://user:pass@172.26.132.238:5984/data/_design/data/_view/test?group=true"
+    try:
+        uResponse = requests.get(uri)
+        Jresponse = uResponse.text
+        data = json.loads(Jresponse)
+        output = data
+    except requests.ConnectionError:
+       return "Connection Error"
+
+    return output
+
 if __name__ == '__main__':
     app.debug=True
     app.run()
+
+
+'''
+login page: http://172.26.132.238:5984/_utils/index.html#login
+'''
