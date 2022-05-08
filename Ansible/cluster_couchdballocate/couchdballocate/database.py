a5b637f1283893141edaa4b28d1af8e0e0613f92
belkok <belkok@hotmail.sg>
2022-05-08 12:35:49 +1000

    Update database.py

--- Ansible/cluster_couchdballocate/couchdballocate/database.py
+++ Ansible/cluster_couchdballocate/couchdballocate/database.py
@@ -31,7 +31,7 @@ except:
 housing = ['house price','housing price','rental price','rent price','mortgage','housing','pay rent','real estate','landlord','tenant'] 
 crypto = ['crypto','btc','bitcoin','dogecoin','doge','etheruem','eth','cryptocurrency','bnb']
 election = ['ausvote', 'auspol','auselectoralcom','election','campaign','vote','federal election','ausvotes2022','morrison','scottmorrisonmp','albomp','alboforpm','australianlabor','scottmorrison']
-covid = ['covid','cov-19','covid-19','coronavirus','covid19']
+covid = ['covid','cov-19','covid-19','coronavirus','covid19','cov19']
 
 crypto_count = 0
 housing_count =0
@@ -48,19 +48,24 @@ for db in db_names:
             if any(k in text for k in housing):
                 db_housing.save({'id': database[id]['id'], 'suburb': database[id]['suburb'], 'text': database[id]['text'], 'sentiment': database[id]['sentiment'], 'score': database[id]['score']})
                 housing_count += 1
-                print("saved to housing")
+                
             if any(k in text for k in crypto):
                 db_crypto.save({'id': database[id]['id'], 'suburb': database[id]['suburb'], 'text': database[id]['text'], 'sentiment': database[id]['sentiment'], 'score': database[id]['score']})
                 crypto_count += 1
-                print("saved to crypto")
+                
             if any(k in text for k in election):
                 db_election.save({'id': database[id]['id'], 'suburb': database[id]['suburb'], 'text': database[id]['text'], 'sentiment': database[id]['sentiment'], 'score': database[id]['score']})
                 election_count += 1
-                print("saved to election")
+                
             if any(k in text for k in covid):
                 db_covid.save({'id': database[id]['id'], 'suburb': database[id]['suburb'], 'text': database[id]['text'], 'sentiment': database[id]['sentiment'], 'score': database[id]['score']})
                 covid_count += 1
-                print("saved to covid")
+                
         except:
             continue
+
 print("tweets allocation done")
+print("crypto: ",crypto_count)
+print("housing: ",housing_count)
+print("covid: ",covid_count)
+print("election: ",election_count)
