61e54c13597dd8890238582b770145145538b7d2
Yuhong-Guo <62046117+Yuhong-Guo@users.noreply.github.com>
2022-05-15 23:24:30 +1000

    more suitable legend bins

--- frontend/src/components/map/mapHousehold.js
+++ frontend/src/components/map/mapHousehold.js
@@ -21,13 +21,13 @@ const MapHousehold = () => {
     }, []);
 
     function getColor(d) {
-        return d > 2800 ? '#800026' :
-               d > 2500  ? '#BD0026' :
-               d > 2200  ? '#E31A1C' :
-               d > 1900  ? '#FC4E2A' :
-               d > 1600   ? '#FD8D3C' :
+        return d > 2050 ? '#800026' :
+               d > 1900  ? '#BD0026' :
+               d > 1750  ? '#E31A1C' :
+               d > 1600  ? '#FC4E2A' :
+               d > 1450   ? '#FD8D3C' :
                d > 1300   ? '#FEB24C' :
-               d > 1000   ? '#FED976' :
+               d > 1150   ? '#FED976' :
                           '#FFEDA0';
     }
     const style = (feature) => {
@@ -80,7 +80,7 @@ const MapHousehold = () => {
             legend.onAdd =  function (map) {
     
             var div = L.DomUtil.create('div', 'info legend'),
-                grades = [0, 1000, 1300, 1600, 1900, 2200, 2500, 2800],
+                grades = [0, 1150, 1300, 1450, 1600, 1750, 1900, 2050],
                 labels = [];
         
             // loop through our density intervals and generate a label with a colored square for each interval
